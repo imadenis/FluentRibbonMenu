dba4207b267085d0a424f2ff3487df60f121edec
CORP\imadenis <ioannis.madenis@sogeti.com>
2025-03-26 17:06:14 +0100

    Demo

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Fluent.RibbonWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextOptions.GetTextFormattingMode(window) == TextFormattingMode.Display)
+                TextOptions.SetTextFormattingMode(window, TextFormattingMode.Ideal);
+            else
+                TextOptions.SetTextFormattingMode(window, TextFormattingMode.Display);
+        }
+    }
+}
